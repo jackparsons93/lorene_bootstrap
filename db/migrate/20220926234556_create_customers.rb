9cb23839b245422adf92cc5903f77479c17e8575
chris wilson <chriswilson@chriss-air.lan>
2022-09-26 18:52:30 -0500

    added customer form

---
+++ db/migrate/20220926234556_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :address
+      t.string :city
+      t.string :zipcode
+
+      t.timestamps
+    end
+  end
+end
