401eb175349a5891c34c607596c3f4f8e8265238
chris wilson <chriswilson@chriss-air.lan>
2022-09-26 18:24:41 -0500

    added homepage

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/services'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
